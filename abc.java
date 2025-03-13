7feabb630f29c7ef998f91d7dc910b5011f2f44b
ssm002 <91481908+ssm002@users.noreply.github.com>
2025-03-13 15:45:45 +0530

    Update abc.java
    added jumbo chimre line

--- abc.java
+++ abc.java
@@ -3,6 +3,7 @@ public static void main(String[] args) {
     System.out.println("AAAAAA");
     System.out.println("Jambuban");
     System.out.println("Chimre");
+    System.out.println("jumbo chimre");
 
 }
 
